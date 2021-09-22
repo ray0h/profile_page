947a846e04349b0c8a7c4501d01e276ea0a585c0
roh linux <raymond.oh@gmail.com>
2021-09-22 14:57:50 -0400

    Added some basic thoughts for layout

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,34 @@
 <template>
   <div id="app">
-    Hi, this is the app.
+    <Navbar></Navbar>
+    <p>Hi, this is the page.</p>
+    <p>What is the color scheme?  Font?  Any distracting animations?</p>
+    <p>Should plan to have small intro with picture</p>
+    <p>Section with several projects</p>
+    <p>Contact Me</p>
+    <p>Make it Responsive (use Tailwind?)</p>
+
   </div>
 </template>
 
 <script>
-
+import Navbar from './components/NavBar'
 export default {
   name: 'App',
   components: {
+    Navbar
   }
 }
 </script>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="navContainer">
+    This is the nav bar area.
+    Home
+    About
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
+
+<style>
+  #navContainer {
+    background-color: lightgray;
+  }
+</style>
