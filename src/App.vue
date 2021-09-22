342d9de22d451f6012fcf0ffd03950663a02121d
roh linux <raymond.oh@gmail.com>
2021-09-22 14:41:56 -0400

    initial vue2 cli setup; removed boilerplate features

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    Hi, this is the app.
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
